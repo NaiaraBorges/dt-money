4dc124a433ddad9a26e1d8a02c53105e0942e309
Naiara <naiara@mentorspace.dev>
2022-10-18 22:20:10 -0300

    Criando tabela de transações

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,47 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Aluguel do apartamento</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>27/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
